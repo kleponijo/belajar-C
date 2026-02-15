b7cd98465831dca9ec6f9793dcd6a6ca22d82657
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-15 21:16:29 +0700

    commit 1

---
+++ auth/login.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "login.h"
+
+loginPage(){
+  printf("halaman login bang \n");
+}
---
+++ page/home.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "home.h"
+
+homePage(){
+  printf("halaman home bang \n");
+}
