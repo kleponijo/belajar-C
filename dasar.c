aad9ba62bb3ba0dfbf1eab3f187def7811c5dcff
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-22 13:30:57 +0700

    oke

--- dasar.c
+++ dasar.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+#define JUDUL "Nadif.com"
+#define BILANGAN 100
+
 void main(){
   int angka = 10;
-  char huruf = "A";
-  float pecahan = 4.5;
-  printf("Hello World!");
+  char huruf = 'A';
+  float pecahan = 4.57234;
+
+  printf("Konstanta JUDUL adalah %s\n", JUDUL);
+  printf("Konstanta BILANGAN adalah %i\n", BILANGAN);
+
+  printf("Variabel angka = %d\n", angka);
+  printf("Variabel huruf = %c\n", huruf);
+  // .2 untuk menentukan hanya ada 2 angka di belakang koma
+  printf("Variabel pecahan = %.2f\n", pecahan);
+  
 }
