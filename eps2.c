28773e904b158ac4acd4e828e5f71a56ab1abe1c
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-23 06:10:59 +0700

    buhhh gila

---
+++ eps2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  // line tab dan new line
+  // tab menggunakan \t dan new line menggunakan \n
+
+  printf("\tHello World! \n\tHallo Teman! ");
+  
+  return 0;
+}
