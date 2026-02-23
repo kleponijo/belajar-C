ec175fba3f664660fe16aa9b768dfc2ef2b274af
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-23 09:30:21 +0700

    ije

---
+++ eps3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int x; // int adalah sebuah tipe data yang memiliki nilai berupa angka
+  float y; // float adalah tipe data yang memiliki nilai berupa angka (angkanya desimal ada koma (.))
+  char z; // char adalah karakter
+  /* cara pengoutputan data ini dengan %
+  */
+  
+  return 0;
+}
