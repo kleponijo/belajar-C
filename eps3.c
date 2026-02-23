b90da43201e0bf7712a169024777dee4b7253ad1
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-23 14:25:38 +0700

    o

--- eps3.c
+++ eps3.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
 int main(){
-  int x; // int adalah sebuah tipe data yang memiliki nilai berupa angka
+  int x, b, c, d; // int adalah sebuah tipe data yang memiliki nilai berupa angka
   float y; // float adalah tipe data yang memiliki nilai berupa angka (angkanya desimal ada koma (.))
-  char z; // char adalah karakter
+  char z, l, q; // char adalah karakter
+  // double
+  // long
+  // short
   /* cara peng-outputan tipe data int dengan %d
      cara peng-outputan tipe data float dengan %f
      cara peng-outputan tipe data char dengan %c
   */
-  x = 10;
-  printf("x = %d", x);
+//  ini untuk int
+  // x = 10;
+  // b = 25;
+  
+  // printf("x = %d \na = %d", x, b);
+
+  // ini untuk float
+  // y = 2.3;
+  // printf("y = %.1f \n", y);
+  // printf("y = %.3f \n", y);
+  // printf("y = %f \n", y);
+
+  //ini untuk char
+  z = 'y';
+  l = 'd';
+  q = 'x';
+  printf("%d", q);
+
 
   return 0;
 }
