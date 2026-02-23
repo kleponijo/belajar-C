fef40112865d301adcb619c6517b96a55d08cdbe
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-23 15:19:17 +0700

    woke

--- eps4.c
+++ eps4.c
@@ -2,8 +2,8 @@
 #include <string.h>
 
 int main(){
-  int x; // untuk int (integer) itu menggunakan %d
-  char y; // untuk char (character) itu menggunakan %c
+  int x, a, hasil; // untuk int (integer) itu menggunakan %d
+  char y, b; // untuk char (character) itu menggunakan %c
   float z; //
 
   // untuk int
@@ -17,10 +17,24 @@ int main(){
   // printf("nilai yang baru anda masukan adalah x = %c", y);
 
   // untuk float
-  printf("masukan ke nilai ke z : ");
-  scanf("%f", &z);
-  printf("nilai yang baru anda masukan adalah x = %f \n", z);
-  printf("nilai yang baru anda masukan adalah x = %.1f", z);
+  // printf("masukan ke nilai ke z : ");
+  // scanf("%f", &z);
+  // printf("nilai yang baru anda masukan adalah x = %f \n", z);
+  // printf("nilai yang baru anda masukan adalah x = %.1f", z);
+
+  // printf("masukan nilai yang akan dijumlahkan : ");
+  // scanf("%d", &x);
+  // printf("masukan nilai selanjutnya : ");
+  // scanf("%d", &a);
+  // hasil = x + a;
+  // printf("%d + %d = %d", x, a, hasil);
+
+  // untuk karakter
+  printf("masukan karakter_1 : ");
+  scanf("%c", &y);
+  printf("masukan karakter_2 : ");
+  scanf(" %c", &b);
+  printf("karakter_ 1 adalah %c\ndan \nkarakter_2 adalah %c", y, b);
 
   return 0;
 }
