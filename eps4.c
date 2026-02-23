33ee749cfc65a6daad465956e2fda9a811f3c393
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-23 14:43:29 +0700

    wow

--- eps4.c
+++ eps4.c
@@ -3,8 +3,24 @@
 
 int main(){
   int x; // untuk int (integer) itu menggunakan %d
-  printf("masukan nilai x : ");
-  scanf("%d", &x);
-  printf("nilai yang baru anda masukan adalah x = %d", x);
+  char y; // untuk char (character) itu menggunakan %c
+  float z; //
+
+  // untuk int
+  // printf("masukan nilai x : ");
+  // scanf("%d", &x);
+  // printf("nilai yang baru anda masukan adalah x = %d", x);
+
+  // untuk char
+  // printf("masukan ke karakter y maksimal 1 char : ");
+  // scanf("%c", &y);
+  // printf("nilai yang baru anda masukan adalah x = %c", y);
+
+  // untuk float
+  printf("masukan ke nilai ke z : ");
+  scanf("%f", &z);
+  printf("nilai yang baru anda masukan adalah x = %f \n", z);
+  printf("nilai yang baru anda masukan adalah x = %.1f", z);
+
   return 0;
 }
