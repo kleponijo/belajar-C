2f54d29f91187fb6ad3f09062658cd51ef68bd8a
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-23 12:26:32 +0700

    Update eps3.c

--- eps3.c
+++ eps3.c
@@ -4,8 +4,12 @@ int main(){
   int x; // int adalah sebuah tipe data yang memiliki nilai berupa angka
   float y; // float adalah tipe data yang memiliki nilai berupa angka (angkanya desimal ada koma (.))
   char z; // char adalah karakter
-  /* cara pengoutputan data ini dengan %
+  /* cara peng-outputan tipe data int dengan %d
+     cara peng-outputan tipe data float dengan %f
+     cara peng-outputan tipe data char dengan %c
   */
-  
+  x = 10;
+  printf("x = %d", x);
+
   return 0;
 }
