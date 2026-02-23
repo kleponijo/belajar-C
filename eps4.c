00b5780802e01b6f0c4a6243a6176b28305597a1
e32232340-Nadif <158453666+kleponijo@users.noreply.github.com>
2026-02-23 14:35:41 +0700

    behh

---
+++ eps4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+  int x; // untuk int (integer) itu menggunakan %d
+  printf("masukan nilai x : ");
+  scanf("%d", &x);
+  printf("nilai yang baru anda masukan adalah x = %d", x);
+  return 0;
+}
